ace4609143c5f633dad8f57c61af885967cb3cd0
Mikkel MFH. Frydenlund Hammer <miha@pc-tec.local>
2019-01-23 14:23:47 +0100

    Added role model and restrictions to models

--- AoApi.Data.Models/Employee.cs
+++ AoApi.Data.Models/Employee.cs
@@ -1,6 +1,7 @@
 ﻿using AoApi.Data.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AoApi.Data.Models
@@ -8,12 +9,18 @@ namespace AoApi.Data.Models
     [Table("Employees")]
     public class Employee : DeleteableEntityModel<Guid>
     {
+        [Required(ErrorMessage = "Employee must have a firstname")]
+        [MaxLength(20, ErrorMessage = "Firstname must not be longer than 20 characters")]
         public string FirstName { get; set; }
+        [MaxLength(30, ErrorMessage = "Lastname must not be longer than 30 characters")]
         public string LastName { get; set; }
         public string Email { get; set; }
-        public DateTime Birthday { get; set; }
+        public DateTimeOffset Birthday { get; set; }
+        [MaxLength(20, ErrorMessage = "City must not be longer than 20 characters")]
         public string City { get; set; }
+        [MaxLength(20, ErrorMessage = "Country must not be longer than 20 characters")]
         public string Country { get; set; }
+        [MaxLength(20, ErrorMessage = "Street must not be longer than 20 characters")]
         public string Street { get; set; }
         public string PhoneNumber { get; set; }
 
--- AoApi.Data.Models/User.cs
+++ AoApi.Data.Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using AoApi.Data.Common;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AoApi.Data.Models
@@ -7,14 +8,24 @@ namespace AoApi.Data.Models
     [Table("Users")]
     public class User : DeleteableEntityModel<Guid>
     {
+        [Required(ErrorMessage = "User must have a username")]
+        [MaxLength(20, ErrorMessage = "Username must not be longer than 20 characters")]
         public string Username { get; set; }
+
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Password must not be longer than 20 characters")]
         public string Password { get; set; }
         public string Email { get; set; }
-        public string Role { get; set; } // change to one-to-many relationship
 
         // employee relation
+        [Required(ErrorMessage = "User must have an employee id")]
         public Guid EmployeeId { get; set; }
         [ForeignKey("EmployeeId")]
         public Employee Employee { get; set; }
+
+        // employee relation
+        [Required(ErrorMessage = "User must have an role id")]
+        public Guid RoleId { get; set; }
+        [ForeignKey("RoleId")]
+        public Role Role { get; set; }
     }
 }
