bf94a0f9fe148ffa003b7536672f7d76741888c6
Mikkel MFH. Frydenlund Hammer <miha@pc-tec.local>
2019-01-24 09:00:10 +0100

    Added empty repos for all classes that only inherit from base repo

---
+++ AoApi.Data/Repositories/ScheduleRepository.cs
@@ -0,0 +1,11 @@
+﻿using AoApi.Data.Models;
+
+namespace AoApi.Data.Repositories
+{
+    public class ScheduleRepository : RepositoryBase<Schedule>
+    {
+        public ScheduleRepository(AOContext context) : base(context)
+        {
+        }
+    }
+}
